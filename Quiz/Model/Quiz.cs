5457cf3b509ebf90bd5c33627503be9a2de92e02
Patryk Gamrat <Patryk.gam@gmail.com>
2024-06-23 17:44:16 +0200

    Bug fix

--- Quiz/Model/Quiz.cs
+++ Quiz/Model/Quiz.cs
@@ -107,10 +107,11 @@ namespace QuizApp.Model
             newQuestion.Content = "Nowe pytanie";
             Questions.Add(newQuestion);
 
-/*            for (int i = 0; i < 4; i++)
+            // Add answers for question
+            for (int i = 0; i < 4; i++)
             {
                 Answers.Add(new Answer(lastId));
-            }*/
+            }
         }
 
         public void removeQuestion(int removeQuestionId)
